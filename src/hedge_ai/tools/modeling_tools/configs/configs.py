e8cb1ec3f9f616cf93baae2f755a7022544ce5ed
medsriha <medsriha@gmail.com>
2025-02-21 17:08:41 -0600

    update scope

--- src/hedge_ai/tools/modeling_tools/configs/configs.py
+++ src/hedge_ai/tools/modeling_tools/configs/configs.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
 from datetime import datetime, timedelta
 
+
 @dataclass
 class CrossoverConfig:
     """Configuration for crossover analysis."""
+
     lower_sma: int = 20
     upper_sma: int = 50
     take_profit: float = 0.10
@@ -13,5 +15,5 @@ class CrossoverConfig:
     rsi_overbought: int = 70
     rsi_oversold: int = 30
     rsi_underbought: int = 50
-    start_date: str = (datetime.now() - timedelta(days=365 + (upper_sma * 2))).strftime("%Y-%m-%d") 
+    start_date: str = (datetime.now() - timedelta(days=365 + (upper_sma * 2))).strftime("%Y-%m-%d")
     end_date: str = datetime.now().strftime("%Y-%m-%d")
