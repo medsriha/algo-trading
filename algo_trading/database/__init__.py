8b4a95bff682f45097dafc2d306e04bf3eca526a
medsriha <medsriha@gmail.com>
2025-03-15 23:23:17 -0500

    Update __init__.py to include CompanyOverviewExtractor and its configuration in the exported module components

--- algo_trading/database/__init__.py
+++ algo_trading/database/__init__.py
@@ -3,5 +3,8 @@ from algo_trading.database.crossover.read import FindCandidateCrossover
 from algo_trading.database.news.configs import FinnhubNewsDbConfig, AlphaVantageNewsDbConfig
 from algo_trading.database.news.finnhub_news import FinnhubNewsExtractor
 from algo_trading.database.news.alphavantage_news import AlphaVantageNewsExtractor
+from algo_trading.database.companies.configs import CompanyOverviewDbConfig
+from algo_trading.database.companies.companies_overview import CompanyOverviewExtractor
 
-__all__ = ["FindCandidateCrossover", "DatabaseCrossoverConfig", "FinnhubNewsDbConfig", "FinnhubNewsExtractor", "AlphaVantageNewsDbConfig", "AlphaVantageNewsExtractor"]
+__all__ = ["FindCandidateCrossover", "DatabaseCrossoverConfig", "FinnhubNewsDbConfig", "FinnhubNewsExtractor", 
+           "AlphaVantageNewsDbConfig", "AlphaVantageNewsExtractor", "CompanyOverviewDbConfig", "CompanyOverviewExtractor"]
